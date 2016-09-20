0352dd109e283cc5538b62fc52ccdae176ad5b82
Christoffer Karlsson <christoffer.karlsson@bekk.no>
2016-09-20 20:51:46 +0200

    Use spaces, not tabs...

--- app/app.py
+++ app/app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return "Working!"
+    return "Working!"
 
 @app.route('/kill-me')
 def kill_app():
-	exit(0)
+    exit(0)
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000')
