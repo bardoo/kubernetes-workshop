76e2f1f1acb132f512285729c077da5460cb571d
Christoffer Karlsson <christoffer.karlsson@bekk.no>
2016-09-26 16:25:02 +0200

    Show IP and allow custom addition to system out log

--- app/app.py
+++ app/app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+import socket
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Working!"
+    log_param = request.args.get('log', None)
+    if log_param:
+        print("LOG: {}".format(log_param))
+
+    return "Working! App v2.<br/>Intern IP: {}".format(get_ip())
+
+@app.route('/ip')
+def get_ip():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.connect(('8.8.8.8', 53))
+    ip = sock.getsockname()[0]
+    sock.close()
+
+    return ip
 
 @app.route('/kill-me')
 def kill_app():
